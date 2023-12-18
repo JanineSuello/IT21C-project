eb7db0a264c4bf407db0a33131800a696c290592
JanineSuello <janinegrasyanasuello@gmail.com>
2023-12-19 07:23:02 +0800

    added package OOP_basics

---
+++ src/OOP_Basics/Favorites.java
@@ -0,0 +1,20 @@
+
+package OOP_Basics;
+
+
+class Favorites {
+    String name;
+    String hobbies;
+    
+    Favorites(String name, String hobbies) {
+        this.name = name;
+        this.hobbies = hobbies;
+  
+    }
+    String getName() {
+        return name;
+    }
+    String getHobbies() {
+        return hobbies;
+    }
+}
---
+++ src/OOP_Basics/Main.java
@@ -0,0 +1,13 @@
+
+package OOP_Basics;
+
+
+public class Main {
+      public static void main(String [] args){
+        Favorites myfav= new Favorites("Grasyana", "Reading and Sleeping");
+        
+        System.out.println("Name: " + myfav.getName());
+        System.out.println("Favorites: " + myfav.getHobbies());
+        
+    }
+}
